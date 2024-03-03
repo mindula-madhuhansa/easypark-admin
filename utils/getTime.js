8c2f5be100bd9c773864ab5a709f99889f366643
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-03-03 22:29:44 +0530

    remove qrscanner

--- utils/getTime.js
+++
@@ -1,7 +0,0 @@
-export const getCurrentTimeFormatted = () => {
-  const time = new Date();
-  return `${time.getHours().toString().padStart(2, "0")}:${time
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}:${time.getSeconds().toString().padStart(2, "0")}`;
-};
